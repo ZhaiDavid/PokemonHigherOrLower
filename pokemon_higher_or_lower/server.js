a9d969712cc0f63c35c35759aa929bee904e445e
ZhaiDavid <david.zhai@student.tdsb.on.ca>
2026-07-04 02:09:01 -0400

    fixed some bugs with matchmaking

--- pokemon_higher_or_lower/server.js
+++ pokemon_higher_or_lower/server.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 import Queue from './Queue.js'
 
 const rooms = new Map();
-const user_socket = new Map();
+let user_socket = new Map();
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
@@ -30,7 +30,6 @@ app.prepare().then(() => {
   io.on("connection", (socket) => {
     const user_id = socket.handshake.auth.userId; 
     console.log(`User connected ${user_id}`);
-    user_socket.set(user_id, socket);
 
     // Handling Matchmaking
     socket.on("match-search", async () => {
@@ -38,14 +37,19 @@ app.prepare().then(() => {
         queue.enqueue(user_id);
       }
 
+      user_socket.set(user_id, socket);
+
+
       // queue.enqueue([userName, socket]);
       // console.log(`${socket.id} (${userName}) is in queue`)
       // console.log(queue.print());
 
       console.log(`${user_id} is in queue`);
-      //console.log(queue.print());
+      console.log(queue.getSize());
+      //console.log(queue.print())
       
       if (queue.getSize() >= 2) {
+        
         // const [username1, socket1] = queue.dequeue();
         // const [username2, socket2] = queue.dequeue();
         
@@ -58,8 +62,13 @@ app.prepare().then(() => {
         //   userName: username2
         // })
         
-        const socket1 = user_socket.get(queue.dequeue());
-        const socket2 = user_socket.get(queue.dequeue());
+        const user_id1 = queue.dequeue();
+        const user_id2 = queue.dequeue();
+        const socket1 = user_socket.get(user_id1);
+        const socket2 = user_socket.get(user_id2);
+        user_socket.delete(user_id1);
+        user_socket.delete(user_id2);
+
         
         socket1.emit("match-found", {
           roomName: createRoomID(socket1.id, socket2.id),
