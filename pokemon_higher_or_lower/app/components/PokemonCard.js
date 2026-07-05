d3cab841d0b4dbd93ce04a79357b586fbb7fa841
IridescentSky <frankhuang132@gmail.com>
2026-07-05 00:48:05 -0400

    small change to text in pokemon card

--- pokemon_higher_or_lower/app/components/PokemonCard.js
+++ pokemon_higher_or_lower/app/components/PokemonCard.js
@@ -1,37 +1,37 @@
 import { colours } from '../constants/pokemon-type-colours.js'
-import { pokemon_types_map } from  '../constants/pokemon-type.js'
+import { pokemon_types_map } from '../constants/pokemon-type.js'
 import "./PokemonCard.css";
 
-export default function PokemonCard({randomPokemon, index, locked, usage, handleImageClick}) {
+export default function PokemonCard({ randomPokemon, index, locked, usage, handleImageClick }) {
     const types = pokemon_types_map.get(randomPokemon.split(" ")
-                        .join("-")
-                        .toLowerCase());
+        .join("-")
+        .toLowerCase());
     const primary_type = types[0].toLowerCase();
     return (
         <>
-            
+
             <div
                 className="card-container text-white"
                 style={{
                     "--type-color": colours.get(primary_type),
-                }}> 
+                }}>
                 <div
                     className='sm:w-72 sm:h-72 md:w-96 md:h-96 card'>
-                    <p>{randomPokemon}</p>
+                    <p className='card-pokemon-label'>{randomPokemon}</p>
 
-                    {index == 0 ? <p>
+                    {index == 0 ? <p className='card-pokemon-usage'>
                         Usage: {(usage * 100).toFixed(2)}%
                     </p> :
-                        <p>
+                        <p className='card-pokemon-usage'>
                             Usage : {!locked ? "???" : `${(usage * 100).toFixed(2)}%`}
                         </p>}
 
 
-                    <img 
+                    <img
                         className='pokemon-image sm:w-56 sm:h-56 md:w-72 md:h-72'
                         onClick={() => {
-                        if (!locked) handleImageClick(randomPokemon);
-                    }}
+                            if (!locked) handleImageClick(randomPokemon);
+                        }}
                         src={`https://www.smogon.com/dex/media/sprites/xy/${randomPokemon
                             .split(" ")
                             .join("-")
