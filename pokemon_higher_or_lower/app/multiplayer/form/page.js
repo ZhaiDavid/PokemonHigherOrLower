fb6bd21fb9c43c7f127fea19fa1f6f1768c885d2
IridescentSky <frankhuang132@gmail.com>
2026-07-23 23:53:43 -0400

    Small visual update for /multiplayer/form page. Still need to figure out adding formats to this page.

--- pokemon_higher_or_lower/app/multiplayer/form/page.js
+++ pokemon_higher_or_lower/app/multiplayer/form/page.js
@@ -1,33 +1,45 @@
 "use client";
-import { useState} from "react";
+import { useState } from "react";
 import { useRouter } from 'next/navigation';
 
+import "./page.css"
+
 export default function Page() {
-  const[userName, setUserName] = useState("");
-  const[roomName, setRoomName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [roomName, setRoomName] = useState("");
   const router = useRouter();
 
   function submit() {
     const query = new URLSearchParams({ roomName, userName }).toString();
     router.push(`/multiplayer?${query}`);
   }
+
+  // TODO: Have to change some logic to figure out how we work out modes for this?
   return (
     <>
-      <form>
-        <input type = "text" 
-               placeholder = "type your name..."
-               onChange = {(event) => {
-                          setUserName(event.target.value);
-                          }}/>
-        <input type = "text" 
-               placeholder = "enter room name..."
-               onChange = {(event) => {
-                          setRoomName(event.target.value);
-                          }}/>
-      </form>
-      <button onClick = {submit}>
-        submit
-      </button>
+      <div className="box">
+        <div className="content-container">
+          <div className="form-container p-10 rounded-md">
+            <form className="flex flex-col items-center">
+              <input type="text"
+                placeholder="type your name..."
+                className="text-center"
+                onChange={(event) => {
+                  setUserName(event.target.value);
+                }} />
+              <input type="text"
+                placeholder="enter room name..."
+                className="mt-2 text-center"
+                onChange={(event) => {
+                  setRoomName(event.target.value);
+                }} />
+            </form>
+            <button className="mt-2" onClick={submit}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
