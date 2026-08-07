3e527dd26297bdeec973156a4717b5e39d65aad0
ZhaiDavid <david.zhai@student.tdsb.on.ca>
2026-08-06 23:48:40 -0400

    fixed form kind of

--- pokemon_higher_or_lower/app/multiplayer/form/page.js
+++ pokemon_higher_or_lower/app/multiplayer/form/page.js
@@ -1,17 +1,18 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from 'next/navigation';
+import { formatsList } from "../../constants/formats.js";
 
 import "./page.css"
 
 export default function Page() {
   const [userName, setUserName] = useState("");
-  const [roomName, setRoomName] = useState("");
+  const [format, setFormat] = useState("gen1ou");
   const router = useRouter();
 
   function submit() {
-    const query = new URLSearchParams({ roomName, userName }).toString();
-    router.push(`/multiplayer?${query}`);
+    const query = new URLSearchParams({ roomName, userName}).toString();
+    router.push(`/multiplayer?${query}&format=${format}`);
   }
 
   // TODO: Have to change some logic to figure out how we work out modes for this?
@@ -27,12 +28,17 @@ export default function Page() {
                 onChange={(event) => {
                   setUserName(event.target.value);
                 }} />
-              <input type="text"
-                placeholder="enter room name..."
-                className="mt-2 text-center"
-                onChange={(event) => {
-                  setRoomName(event.target.value);
-                }} />
+              <div className="mt-3 flex flex-col items-center">
+              <label className="" htmlFor="formats">Choose a Format</label>
+              <select className="mt-2" id="formats" name="format"
+                      onChange={(event)=> {
+                        setFormat(event.target.value);
+                      }}>
+                {formatsList.map((element, index) => (
+                  <option key={index} value={element}>{element}</option>
+                ))}
+              </select>
+            </div>
             </form>
             <button className="mt-2" onClick={submit}>
               Submit
