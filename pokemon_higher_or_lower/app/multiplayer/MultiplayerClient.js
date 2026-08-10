22ae1737ffcb6a08b3ca154d34306e03942cc2c4
ZhaiDavid <david.zhai@student.tdsb.on.ca>
2026-08-09 21:34:10 -0400

    fixed issue with useSearchParams during deployment

---
+++ pokemon_higher_or_lower/app/multiplayer/MultiplayerClient.js
@@ -0,0 +1,75 @@
+"use client"
+import GameComponent from './_components/GameComponent';
+import ResultComponent from './_components/ResultComponent';
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { socket } from "../socket/socketClient";
+
+export default function MultiplayerClient () {
+  const params = useSearchParams();
+  const [pokemons, setPokemons] = useState(null);
+  const [playerScore, setPlayerScore] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [roomScores, setRoomScores] = useState(null);
+  const [playerUsernames, setPlayerUserNames] = useState(null)
+  const [userID, setUserID] = useState(null);
+
+  const roomName = params.get("roomName");
+  const userName = params.get("userName");
+  const format = params.get("format");
+
+  const [gameFull, setGameFull] = useState(false);
+
+  useEffect(() => {
+    socket?.emit("joined-room", {
+      roomName: roomName,
+      userName: userName,
+      numPokemon: 2,
+      format: format
+    });
+
+    const handle_room_state = ({pokemons, pokemonData, playerScore, roomScores, playerUsernames}) => {
+      setPokemons(pokemons);
+      setPokemonData(pokemonData);
+      setPlayerScore(playerScore);
+      setRoomScores(roomScores);
+      setPlayerUserNames(playerUsernames);
+    };
+
+    const handleUserID = ({userID}) => {
+      setUserID(userID);
+    }
+    
+    const handleGameFull = () => {
+      setGameFull(true);
+    }
+
+    socket?.on("game-full", handleGameFull);
+    socket?.on("room-state", handle_room_state);
+    socket?.on("user-id", handleUserID);
+
+    return () => {
+        socket?.off("game-full", handleGameFull);
+        socket?.off("room-state", handle_room_state);
+        socket?.off("user-id", handleUserID);
+    };
+  }, []);
+
+
+  return (
+    <>
+      {pokemons && !gameFull && <GameComponent
+        startingPokemons = {pokemons}
+        pokemonData = {pokemonData}
+        roomName = {roomName}
+        playerScore = {playerScore}
+        roomScore = {roomScores}
+        playerUsernames={playerUsernames}
+        format={format}
+        userID={userID}
+      />}
+      {gameFull && <div className='flex justify-center items-center flex-grow-1 text-4xl md:text-5xl font-bold'>Game Full</div>}
+    </>
+  )
+}
--- pokemon_higher_or_lower/app/multiplayer/page.js
+++ pokemon_higher_or_lower/app/multiplayer/page.js
@@ -1,76 +1,10 @@
-"use client"
-import GameComponent from './_components/GameComponent';
-import ResultComponent from './_components/ResultComponent';
+import { Suspense } from "react";
+import MultiplayerClient from "./MultiplayerClient";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState, useRef } from "react";
-import { socket } from "../socket/socketClient";
-
-export default function Page () {
-  const params = useSearchParams();
-  const [pokemons, setPokemons] = useState(null);
-  const [playerScore, setPlayerScore] = useState(null);
-  const [pokemonData, setPokemonData] = useState([]);
-  const [roomScores, setRoomScores] = useState(null);
-  const [playerUsernames, setPlayerUserNames] = useState(null)
-  const [userID, setUserID] = useState(null);
-
-  const roomName = params.get("roomName");
-  const userName = params.get("userName");
-  const format = params.get("format");
-
-  const [gameFull, setGameFull] = useState(false);
-
-  // for some reason useEffect runs twice
-  useEffect(() => {
-    socket.emit("joined-room", {
-      roomName: roomName,
-      userName: userName,
-      numPokemon: 2,
-      format: format
-    });
-
-    const handle_room_state = ({pokemons, pokemonData, playerScore, roomScores, playerUsernames}) => {
-      setPokemons(pokemons);
-      setPokemonData(pokemonData);
-      setPlayerScore(playerScore);
-      setRoomScores(roomScores);
-      setPlayerUserNames(playerUsernames);
-    };
-
-    const handleUserID = ({userID}) => {
-      setUserID(userID);
-    }
-    
-    const handleGameFull = () => {
-      setGameFull(true);
-    }
-
-    socket.on("game-full", handleGameFull);
-    socket.on("room-state", handle_room_state);
-    socket.on("user-id", handleUserID);
-
-    return () => {
-        socket.off("game-full", handleGameFull);
-        socket.off("room-state", handle_room_state);
-        socket.off("user-id", handleUserID);
-    };
-  }, []);
-
-
+export default function Page() {
   return (
-    <>
-      {pokemons && !gameFull && <GameComponent
-        startingPokemons = {pokemons}
-        pokemonData = {pokemonData}
-        roomName = {roomName}
-        playerScore = {playerScore}
-        roomScore = {roomScores}
-        playerUsernames={playerUsernames}
-        format={format}
-        userID={userID}
-      />}
-      {gameFull && <div className='flex justify-center items-center flex-grow-1 text-4xl md:text-5xl font-bold'>Game Full</div>}
-    </>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <MultiplayerClient />
+    </Suspense>
+  );
 }
